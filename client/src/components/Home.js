8ae979a0ff914caf4c7a641bfcf40a2adfbf987e
= <abhishek.sankar.in@gmail.com>
2019-10-17 10:04:20 +0530

    CSS Chages to profile.js and home.js

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -35,32 +35,40 @@ class Home extends Component {
 		  }
   render() {
     return (
+	
 		<div className="body">
       		<div className="container" >
-        		<div className="jumbotron mt-5 bg-dark">
+        		<div className="jumbotron mt-5 bg-light sizeAdjuster" >
           			<div className="col-sm-5 mx-auto">
-            			<h1 className="text-center bg-dark text-light">Welcome {this.state.first_name}</h1>
+            			<h1 className="text-center text-dark">Welcome {this.state.first_name}</h1>
           			</div>
         		</div>
       		</div>
 			  <div className="container" >
-        		<div className="jumbotron mt-5 bg-dark">
-				<div className="col-sm-12">
-            			<h1 className="text-center bg-dark text-light">Today</h1>
-						<button type="button" class="btn btn-primary m-4">Physics</button>
-						<button type="button" class="btn btn-secondary m-4">Data Structures</button>
-						<button type="button" class="btn btn-success m-4">Engineering Mathematics</button>
-						<button type="button" class="btn btn-info m-4">Life Skills</button>
+        		<div className="jumbotron mt-5 bg-light sizeAdjuster">
+				<div class="col-sm-12" style={{marginRight: 'auto', marginLeft: 'auto'}}>
+            			<h1 className="text-center text-dark">Today's timetable</h1><br></br>
+						<div class="row" className="text-center">
+						<button type="button" className = "colorThemeSet" class="btn btn-secondary m-1 col-sm-3">Physics<br></br><br></br>9:00PM to 10:00PM</button>
+						<button type="button" class="btn btn-secondary m-1 col-sm-3">Data Structures<br></br><br></br>10:00PM to 11:00PM</button>
+						<button type="button" class="btn btn-secondary m-1 col-sm-3">Engineering Mathematics<br></br><br></br>11:00PM to 12:00PM</button>
+						</div>
+						{/* <button type="button" class="btn btn-primary m-4">Lsmc2 break<br></br><br></br>12:00PM to 1:00PM</button><br></br> */}
+						<div class="row" className="text-center">
+						<button type="button" class="btn btn-secondary m-1 col-sm-3">Life Skills<br></br><br></br>1:00PM to 2:00PM</button>
+						<button type="button" class="btn btn-secondary m-1 col-sm-3">Data Structures<br></br><br></br>2:00PM to 3:00PM</button>
+						<button type="button" class="btn btn-secondary m-1 col-sm-3">Engineering Mathematics<br></br><br></br>3:00PM to 4:00PM</button>
+						</div>
           			</div>
         		</div>
-      		</div>\<div className="container" >
-        		<div className="jumbotron mt-5 bg-dark">
+      		</div>
+			  <div className="container" >
+        		<div className="jumbotron mt-5 bg-light sizeAdjuster">
 				<div className="col-sm-12">
-            			<h1 className="text-center bg-dark text-light">Assignments</h1>
-						<button type="button" class="btn btn-primary m-4">Physics</button>
-						<button type="button" class="btn btn-secondary m-4">Data Structures</button>
-						<button type="button" class="btn btn-success m-4">Engineering Mathematics</button>
-						<button type="button" class="btn btn-info m-4">Life Skills</button>
+            			<h1 className="text-center text-dark">Assignments</h1>
+						<button type="button" class="btn btn-secondary m-4 ">Database Management<br></br><br></br>Due Today</button>
+						<button type="button" class="btn btn-secondary m-4">Data Structures<br></br><br></br>Due tomorrow</button>
+						<button type="button" class="btn btn-secondary m-4">Engineering Mathematics<br></br><br></br>Due in 2 days</button>
           			</div>
         		</div>
       		</div>
